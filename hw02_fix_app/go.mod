8c651d256d1221b9cd38a15b3cba84d6fc314583
Ilya Shapkin <ilya.shapkin@rt.ru>
2023-12-25 23:15:57 +0300

    Right modname

--- hw02_fix_app/go.mod
+++ hw02_fix_app/go.mod
@@ -1,3 +1,5 @@
 module github.com/ishapkin/home_work_basic_ishapkin/hw02_fix_app
 
 go 1.20
+
+require github.com/ishapkin/home_work_basic_ishapkin v0.0.0-20231225192444-5e59d243d633 // indirect
--- hw02_fix_app/reader/pkg.go
+++ hw02_fix_app/reader/pkg.go
@@ -6,7 +6,7 @@ import (
 	"io"
 	"os"
 
-	"github.com/fixme_my_friend/hw02_fix_app/types"
+	"github.com/ishapkin/home_work_basic_ishapkin/hw02_fix_app/types"
 )
 
 func ReadJSON(filePath string) ([]types.Employee, error) {
