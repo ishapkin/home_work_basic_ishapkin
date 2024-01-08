f237f36fff1ca2aeeb7f9365d92c38b425b9c23f
Ilya Shapkin <ilya.shapkin@rt.ru>
2024-01-08 22:41:26 +0300

    HW3 is completed

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 func main() {
-	// Place your code here.
+	var size int
+	fmt.Println("Введите размерность доски:")
+	fmt.Scanf("%d\n", &size)
+
+	loopSize := int(2 * math.Pow(float64(size), 2))
+	for i := 1; i <= loopSize; i++ {
+		if i%2 == 0 {
+			if i%(loopSize/size) == 0 {
+				fmt.Println("#")
+			} else {
+				fmt.Print("#")
+			}
+		} else {
+			fmt.Print("  ")
+		}
+	}
 }
