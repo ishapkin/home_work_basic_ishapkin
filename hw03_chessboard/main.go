d27e1c9d7e23149414871edbf91de8f710647f00
Ilya Shapkin <ilya.shapkin@rt.ru>
2024-01-09 15:26:34 +0300

    Resolve converstations

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -5,20 +5,30 @@ import (
 	"math"
 )
 
+func getSquare(blackCase bool) string {
+	if blackCase {
+		return "#"
+	}
+	return " "
+}
+
 func main() {
 	var size int
+	blackMode := false
 	fmt.Println("Введите размерность доски:")
 	fmt.Scanf("%d", &size)
-	loopSize := int(2 * math.Pow(float64(size), 2))
+	loopSize := int(math.Pow(float64(size), 2))
 	for i := 1; i <= loopSize; i++ {
+		blackMode = !blackMode
 		if i%2 == 0 {
 			if i%(loopSize/size) == 0 {
-				fmt.Println("#")
+				fmt.Println("|" + getSquare(blackMode) + "|")
+				blackMode = !blackMode
 			} else {
-				fmt.Print("#")
+				fmt.Print("|" + getSquare(blackMode))
 			}
 		} else {
-			fmt.Print("  ")
+			fmt.Print("|" + getSquare(blackMode))
 		}
 	}
 }
