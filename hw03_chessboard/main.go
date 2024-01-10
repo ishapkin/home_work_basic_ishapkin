3680eb69407341dfc7ee412b7ce15aee61ad196c
Ilya Shapkin <ilya.shapkin@rt.ru>
2024-01-10 13:34:34 +0300

    Kill commnets

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -15,16 +15,13 @@ func getSquare(isBlack *bool) string {
 func main() {
 	var size int
 	isBlack := false
-	fmt.Println("Введите размерность доски:")
+	fmt.Println("Введите размер доски:")
 	fmt.Scanf("%d", &size)
-	// Обход строк
 	for row := 1; row <= size; row++ {
-		// Обход колонок
 		for col := 1; col < size; col++ {
 			fmt.Print("|" + getSquare(&isBlack))
 		}
 		fmt.Println("|" + getSquare(&isBlack) + "|")
-		// Если количетсво четное - то дополнительно переключаем тип клетки
 		if size%2 == 0 {
 			isBlack = !isBlack
 		}
